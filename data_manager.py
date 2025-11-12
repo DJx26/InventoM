90b826ea56656467d6d98dbcb3289f3610d9f1d1
Daksh Jain <dakshpersonal11@gmail.com>
2025-11-12 14:40:34 +0530

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -102,6 +102,7 @@ class DataManager:
                 templates_df = pd.DataFrame(columns=self.templates_headers)
                 templates_df.to_csv(self.templates_file, index=False)
     
+    @st.cache_data(ttl=600)
     def _read_transactions(self) -> pd.DataFrame:
         """Read transactions from Google Sheets or CSV."""
         if self._get_use_sheets():
@@ -125,7 +126,8 @@ class DataManager:
             )
         else:
             df.to_csv(self.transactions_file, index=False)
-    
+   
+    @st.cache_data(ttl=600)
     def _read_stock(self) -> pd.DataFrame:
         """Read stock from Google Sheets or CSV."""
         if self._get_use_sheets():
